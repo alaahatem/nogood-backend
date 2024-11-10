f6f7c16988f5da1493b4e7668ce6b55db74377fe
alaahatem <alaa.kordy@statsbomb.com>
2024-11-10 15:31:52 +0200

    chore: clean up and add dto transformer (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS for all origins (or specify which origins to allow)
   app.enableCors({
-    origin: 'http://localhost:3001',  // Replace with your client URL
-    methods: 'GET, POST',             // Allowed methods
-    allowedHeaders: 'Content-Type',   // Allowed headers
+    origin: 'http://localhost:3001', // Replace with your client URL
+    methods: 'GET, POST', // Allowed methods
+    allowedHeaders: 'Content-Type', // Allowed headers
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // This enables transformation of types in your DTOs
+      whitelist: true, // This removes any properties not defined in your DTO
+      // forbidNonWhitelisted: true, // Optional: throws an error if unknown properties are included
+    }),
+  );
 
   await app.listen(3000);
 }
